1bb9e1b2652e4e063ebbb59b6f5ccb0ea2a760e0
Qmy <powerqmy@163.com>
2016-05-28 15:40:57 +0800

    First Commit

---
+++ WebStart/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebStart.MvcApplication" Language="C#" %>
---
+++ WebStart/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebStart.Startup))]
+namespace WebStart
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
